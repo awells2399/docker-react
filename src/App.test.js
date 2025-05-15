01dc1ce0dd17324b6eb2c63853964dfe29684359
root <root@BlackPanther.>
2025-05-15 15:01:12 -0400

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+it("renders without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).toBeTruthy();
+});
+
+it("renders without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).toBeTruthy();
+});
+
+it("renders without crashing", () => {
+  const { container } = render(<App />);
+  expect(container).toBeTruthy();
+});
